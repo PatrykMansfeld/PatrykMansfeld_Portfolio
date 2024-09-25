63afdd0e11323566b5fe119fa7a26389dd8ceb29
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-09-25 08:27:35 +0200

    changes to welcomepage

---
+++ PatrykMansfeld_Portfolio/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+export default function Navigation() {
+  return (
+    <div>
+      <div>
+        <h1>Navigation</h1>
+        <ul>
+            <li>
+                <a href="#about">About Me</a>
+            </li>
+            <li>
+                <a href="#projects">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ PatrykMansfeld_Portfolio/src/pages/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+function WelcomePage() {
+  return (
+    <div>
+      <div>
+        <Navigation />
+      </div>
+    </div>
+  );
+}
+
+export default WelcomePage;
